618aceabc6333eca2616575f029d912e217c695c
Zuri Salim <zhurysalim@gmail.com>
2025-10-03 21:50:49 +0300

    Initial commit

---
+++ public/pgsql.sql
@@ -0,0 +1,17 @@
+zuri-ict-consultancy/
+│
+├── public/
+│   └── index.html
+│
+├── src/
+│   ├── components/
+│   │   ├── Header.js
+│   │   ├── Main.js
+│   │   └── Footer.js
+│   │
+│   ├── App.js
+│   ├── index.js
+│   └── index.css   (optional)
+│
+├── package.json
+└── node_modules/
