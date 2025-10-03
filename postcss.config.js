38548beacba8f7bdef3c934daf07df012921e02c
zhurysalim-oss <zhurysalim@gmail.com>
2025-10-03 19:21:55 +0300

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,14 @@
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
+```json name=package.json url=https://github.com/zhurysalim-oss/zuri-ict-consultancy/blob/main/package.json
+{
+  "name": "zuri-ict-consultancy",
+  "version": "1.0.0",
+  "description": "A project for Zuri ICT Consultancy",
+  "homepage": "https://zhurysalim-oss.github.io/zuri-ict-consultancy/",
+  "scripts": {
+    "deploy": "gh-pages -d build"
   },
-};
+  "dependencies": {
+    "gh-pages": "^3.2.0"
+  }
+}
+```
